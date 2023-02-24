3ead4087c3a429742b7881812b8ac5598a4e1371
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-24 20:16:23 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount bankAccount = new BankAccount(20000);
+        while (true) {
+            try {
+                bankAccount.withDraw(6000);
+                System.out.println("Снято 6000 сом. Остаток на счете: " + bankAccount.getAmount() + " сом.");
+            } catch (LimitException e) {
+                System.out.println("Недостаточно денег на счете. Остаток на счете: " + e.getRemainingAmount() + " сом.");
+                try {
+                    bankAccount.withDraw(e.getRemainingAmount());
+                    System.out.println("Снято оставшееся на счете: " + e.getRemainingAmount() + " сом.");
+                } catch (LimitException ex) {
+                    System.out.println("Произошла ошибка при снятии денег: " + ex.getMessage());
+                }
+                break;
+            }
+        }
     }
 }
